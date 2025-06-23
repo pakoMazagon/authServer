74e79899fd8f8693f5a13c7be3c8a836f831d7fe
FRC <pako.rc10.4@gmail.com>
2025-06-23 20:51:03 +0200

    habria que descomentar el auth-server que es el nombre del container en docker (si se quiere levantar desde docker)

--- auth/src/main/java/com/tpv/auth/config/SecurityConfig.java
+++ auth/src/main/java/com/tpv/auth/config/SecurityConfig.java
@@ -196,7 +196,7 @@ public class SecurityConfig {
 
     @Bean
     public AuthorizationServerSettings authorizationServerSettings() {
-        return AuthorizationServerSettings.builder().build();
+        return AuthorizationServerSettings.builder()/*.issuer("http://auth-server:9000")*/.build();
     }
 
 }
