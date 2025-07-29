e362606476b5b326b571404d0cfdd5d12f3e6a36
FRC <pako.rc10.4@gmail.com>
2025-07-29 21:31:09 +0200

    para mail a cada usuario

--- auth/src/main/java/com/tpv/auth/infrastructure/entities/clients/ClientEntity.java
+++ auth/src/main/java/com/tpv/auth/infrastructure/entities/clients/ClientEntity.java
@@ -14,7 +14,9 @@ import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.Set;
@@ -28,39 +30,44 @@ import java.util.stream.Collectors;
 @AllArgsConstructor
 @Table(name = "client")
 public class ClientEntity {
-	@Id
-	private UUID id;
-	private String clientId;
-	private String clientSecret;
-	private String clientName;
-	// @Column(length = 1000)
-	@Convert(converter = ClientAuthenticationMethodSetConverter.class)
-	private Set<ClientAuthenticationMethod> authenticationMethods;
-	// @Column(length = 1000)
-	@Convert(converter = AuthorizationGrantTypeSetConverter.class)
-	private Set<AuthorizationGrantType> authorizationGrantTypes;
-	// @Column(length = 1000)
-	private String redirectUris;
-	// @Column(length = 1000)
-	private String scopes;
-	// @Column(length = 2000)
-	private String clientSettings;
-	// @Column(length = 2000)
-	private boolean requireProofKey;
+    @Id
+    private UUID id;
+    private String clientId;
+    private String clientSecret;
+    private String clientName;
+    // @Column(length = 1000)
+    @Convert(converter = ClientAuthenticationMethodSetConverter.class)
+    private Set<ClientAuthenticationMethod> authenticationMethods;
+    // @Column(length = 1000)
+    @Convert(converter = AuthorizationGrantTypeSetConverter.class)
+    private Set<AuthorizationGrantType> authorizationGrantTypes;
+    // @Column(length = 1000)
+    private String redirectUris;
+    // @Column(length = 1000)
+    private String scopes;
+    // @Column(length = 2000)
+    private String clientSettings;
+    // @Column(length = 2000)
+    private boolean requireProofKey;
 
-	public static RegisteredClient toRegisteredClient(ClientEntity clientEntity) {
-		final Set<String> redirectUriSet = Arrays.stream(clientEntity.getRedirectUris().split(",")).map(String::trim)
-				.collect(Collectors.toSet());
-		final Set<String> scopesSet = Arrays.stream(clientEntity.getScopes().split(",")).map(String::trim)
-				.collect(Collectors.toSet());
-		final RegisteredClient.Builder builder = RegisteredClient.withId(clientEntity.getClientId())
-				.clientId(clientEntity.getClientId()).clientSecret(clientEntity.getClientSecret())
-				.clientIdIssuedAt(new Date().toInstant())
-				.clientAuthenticationMethods(am -> am.addAll(clientEntity.getAuthenticationMethods()))
-				.authorizationGrantTypes(agt -> agt.addAll(clientEntity.getAuthorizationGrantTypes()))
-				.redirectUris(ru -> ru.addAll(redirectUriSet)).scopes(sc -> sc.addAll(scopesSet))
-				.clientSettings(ClientSettings.builder().requireProofKey(clientEntity.requireProofKey)
-						.requireAuthorizationConsent(true).build());
-		return builder.build();
-	}
+    public static RegisteredClient toRegisteredClient(ClientEntity clientEntity) {
+        final Set<String> redirectUriSet = Arrays.stream(clientEntity.getRedirectUris().split(",")).map(String::trim)
+                .collect(Collectors.toSet());
+        final Set<String> scopesSet = Arrays.stream(clientEntity.getScopes().split(",")).map(String::trim)
+                .collect(Collectors.toSet());
+        final RegisteredClient.Builder builder = RegisteredClient.withId(clientEntity.getClientId())
+                .clientId(clientEntity.getClientId()).clientSecret(clientEntity.getClientSecret())
+                .clientIdIssuedAt(new Date().toInstant())
+                .clientAuthenticationMethods(am -> am.addAll(clientEntity.getAuthenticationMethods()))
+                .authorizationGrantTypes(agt -> agt.addAll(clientEntity.getAuthorizationGrantTypes()))
+                .redirectUris(ru -> ru.addAll(redirectUriSet)).scopes(sc -> sc.addAll(scopesSet))
+                .clientSettings(ClientSettings.builder().requireProofKey(clientEntity.requireProofKey)
+                        .requireAuthorizationConsent(true).build())
+                .tokenSettings(TokenSettings.builder()
+                        .accessTokenTimeToLive(Duration.ofHours(10))
+                        .refreshTokenTimeToLive(Duration.ofHours(10))
+                        .reuseRefreshTokens(true)
+                        .build());
+        return builder.build();
+    }
 }
